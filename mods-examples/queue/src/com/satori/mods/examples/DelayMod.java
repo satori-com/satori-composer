2e4613be834622a6c090fe3d930fa32b39dbd643
akolomentsev <akolomentsev@satori.com>
2017-08-30 19:49:55 -0700

    examples: queue mod example

---
+++ mods-examples/queue/src/com/satori/mods/examples/DelayMod.java
@@ -0,0 +1,31 @@
+package com.satori.mods.examples;
+
+
+import com.satori.mods.core.async.*;
+import com.satori.mods.suite.*;
+
+import com.fasterxml.jackson.databind.*;
+
+public class DelayMod extends Mod {
+  public final static long defaultDelay = 1000;
+  
+  private final long delay;
+  
+  public DelayMod() {
+    this(defaultDelay);
+  }
+  
+  public DelayMod(long delay) {
+    this.delay = delay;
+  }
+  
+  @Override
+  public void onInput(String inputName, JsonNode data, IAsyncHandler cont) throws Exception {
+    yield(data, AsyncPromise.from(
+      () -> timer(delay, cont::succeed), // success continuation
+      cont::fail // failure continuation
+    ));
+  }
+  
+}
+
