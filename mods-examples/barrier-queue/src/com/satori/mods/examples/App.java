71d61e1380a4894a82ac7b75aefa6ae9633412b6
akolomentsev <akolomentsev@satori.com>
2017-08-31 15:36:01 -0700

    examples: barrier mod examples

---
+++ mods-examples/barrier-queue/src/com/satori/mods/examples/App.java
@@ -0,0 +1,23 @@
+package com.satori.mods.examples;
+
+import com.satori.composer.runtime.*;
+import com.satori.mods.resources.*;
+
+import java.io.*;
+import java.util.logging.*;
+
+public class App {
+  
+  // entry point
+  
+  public static void main(String... args) throws Exception {
+    ComposerRuntime.prepare();
+    try (final InputStream is = ModResourceLoader.loadAsStream("log.properties")) {
+      LogManager.getLogManager().readConfiguration(is);
+    }
+    ComposerRuntime.start(ModResourceLoader.loadAsConfigAndValidate(
+      "config.json", ComposerRuntimeConfig.class
+    ));
+  }
+}
+
