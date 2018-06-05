1ed27deff69d2d40b48cc284e084499fc42ce2fb
akolomentsev <akolomentsev@satori.com>
2018-06-05 15:13:28 -0700

    upgrade gradle to v4.8

--- libs/gradle/codegen/tests/src/com/satori/libs/gradle/codegen/CodeGenPluginTests.kt
+++ libs/gradle/codegen/tests/src/com/satori/libs/gradle/codegen/CodeGenPluginTests.kt
@@ -15,7 +15,8 @@ class CodeGenPluginTests : Assert() {
   @Test
   public fun `validate project extentions`() {
     val project = ProjectBuilder.builder().build()
-    project.pluginManager.apply("com.satori.codegen")
+    //project.pluginManager.apply("com.satori.codegen")
+    project.pluginManager.apply(CodeGenPlugin::class.java)
     
     assertSame(
       ProjectExecTask::class.java,
